ee4b038672fd029b7e876611a52f13f60226be99
Anxiovy <114663739+Anxiovy@users.noreply.github.com>
2022-12-09 12:37:51 +0100

    tarea sinco
    creo q el último comentario de script no funciona XD

---
+++ tarefa5/escriptito.js
@@ -0,0 +1,21 @@
+/*var nome;
+nome= prompt("Como te llamas?");
+alert("Guau... que nome máis interesante " + nome);
+alert("Ti es unha grande persoa " + nome);
+alert("Vales moito " + nome);
+alert("Non hai ninguén máis elegante ca " + nome);
+alert("e... " + nome + ", o mundo adórate!");*/
+
+var ducias;
+ducias= prompt("cuantas docenas de huevos tiene tu tortilla?");
+var numerito= parseInt(ducias);
+alert(numerito+" ok");
+var ovos=numerito +  " eso es un total de "+numerito * 12+" Huevos";
+alert(ovos)
+
+/*var ducias;
+ducias= prompt("cuantos Euros tienes?");
+var numerito= parseFloat(ducias);
+alert(numerito+" ok");
+var ovos= numerito*1.05;
+alert(ovos.tofixed(2));*/
